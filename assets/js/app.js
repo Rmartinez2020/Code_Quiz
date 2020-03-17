c6626502a03135bf7479ee03fa632f730b213812
Roger <rmartinez5926.rm@gmail.com>
2020-03-17 08:53:54 -0400

    set up html layout

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+//Hook into elements
+//===================================================
+//Buttons
+var startGame = $("#start-game");
+var startOver = $("#start-over");
+//Elements to be changed
