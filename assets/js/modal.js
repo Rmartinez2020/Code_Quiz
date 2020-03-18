9b7c2ef1a2b9482face50a7b2e89bf5f7b9653aa
Roger <rmartinez5926.rm@gmail.com>
2020-03-18 09:50:24 -0400

    refactor modal to jquery

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,20 +1,21 @@
+//Target Modal that will show
 const modal = $("#highscoresModal");
 
-// Get the button that opens the modal
-const btn = document.getElementById("highscores");
+// Target button to open modal
+const highscoresBtn = $("#highscores");
 
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+// close button for modal
+const span = $(".close");
 
-// When the user clicks the button, open the modal 
-btn.onclick = function () {
+// open modal when highscores is clicked
+highscoresBtn.on("click", function () {
     modal.css("display", "block")
-}
+})
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
+// close button for modal
+span.on("click",  function () {
     modal.css("display", "none")
-}
+})
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
